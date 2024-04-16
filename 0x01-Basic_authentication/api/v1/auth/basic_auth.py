4dffe48aafae6687a745062c4c6bf0901d74fb90
nokekip <kipnoke@gmail.com>
2024-04-17 01:42:01 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,3 +21,17 @@ class BasicAuth(Auth):
                 authorization_header, str) and authorization_header.startswith(
                     "Basic "):
             return authorization_header[6:] 
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """
+        Decodes a base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
