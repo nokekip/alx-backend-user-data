b33626e033ac93599c3a1ab77c3f286196ef1daa
nokekip <kipnoke@gmail.com>
2024-04-17 01:31:56 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic authentication module
+"""
+
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+    pass
