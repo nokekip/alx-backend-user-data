fffac53e8d1b85171b46c24c976d2a2644b688d9
nokekip <kipnoke@gmail.com>
2024-04-18 21:30:01 +0300

    fix pycodestyle

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,7 +27,7 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    
+
     if user_id == "me":
         if request.current_user is None:
             abort(404)
