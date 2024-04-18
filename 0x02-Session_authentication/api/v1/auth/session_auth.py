15f41e34afd205744352b963d88c5ea086c9d5af
nokekip <kipnoke@gmail.com>
2024-04-18 21:44:27 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for the SessionAuth class
+"""
+
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class
+    """
+    pass
