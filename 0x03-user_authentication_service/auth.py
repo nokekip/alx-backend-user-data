676b5746ffc717df717d01ca32fd4fff513d7c0c
nokekip <kipnoke@gmail.com>
2024-04-25 23:25:56 +0300

    generate uuids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -14,6 +15,13 @@ def _hash_password(password: str) -> bytes:
     Hashes a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    Generates a UUID
+    """
+    return str(uuid4())
 
 
 class Auth:
