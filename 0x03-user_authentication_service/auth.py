ffedfe149af5e5425380bb2d7f5ff116776d1309
nokekip <kipnoke@gmail.com>
2024-04-25 17:22:00 +0300

    hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+auth module
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
